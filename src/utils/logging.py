4599571241bdd94e9483a7895cf7c38bbced2b68
Dewith <dewithmiramon@gmail.com>
2024-03-12 01:31:38 -0500

    Minor changes in logging func

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -3,15 +3,23 @@
 import logging
 import os
 import sys
+import datetime
 
 
 def get_logger():
     """Get the logger for the script."""
-    file_name = os.path.basename(sys.argv[0]).replace(".py", "")
-    file_handler = logging.FileHandler(filename=f"logs/{file_name}.log")
+    script_name = os.path.basename(sys.argv[0])
+    file_name = script_name.replace(".py", "")
+    timestamp = datetime.datetime.now().strftime("%y%m%d")
+    log_path = f"logs/{timestamp}_{file_name}.log"
+
+    file_handler = logging.FileHandler(filename=log_path, mode="a")
     stdout_handler = logging.StreamHandler(stream=sys.stdout)
     handlers = [file_handler, stdout_handler]
-    format_ = "%(asctime)s L%(lineno)s %(levelname)s %(message)s"
-    logging.basicConfig(level=logging.INFO, format=format_, handlers=handlers)
-    logger = logging.getLogger(file_name)
-    return logger
+
+    log_fmt = "%(asctime)s %(filename)s:%(lineno)s %(levelname)s %(message)s"
+    date_fmt = "%Y-%m-%d %H:%M:%S"
+    logging.basicConfig(
+        level=logging.INFO, format=log_fmt, datefmt=date_fmt, handlers=handlers
+    )
+    return logging.getLogger(file_name)
